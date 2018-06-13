4254bdeea60fb507fd91a9cebaca19a5d7635ef8
wuYin <wuYinPost@gmail.com>
2018-06-13 17:02:36 +0800

    添加生成随机数组的函数

---
+++ utils.go
@@ -0,0 +1,18 @@
+package algorithms
+
+import (
+	"math/rand"
+	"time"
+)
+
+//
+// 获取 n 个 [0, max] 元素组成的数组
+//
+func GetArr(n, max int) []int {
+	rand.Seed(time.Now().UnixNano())
+	arr := make([]int, n)
+	for i := 0; i < n-1; i++ {
+		arr[i] = rand.Intn(max + 1)
+	}
+	return arr
+}
