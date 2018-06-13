6f27c8633d2469932f046297cabf58591be0dfc4
wuYin <wuYinPost@gmail.com>
2018-06-13 22:13:47 +0800

    插入排序实现

---
+++ sort/insert_sort/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"algorithms"
+	"fmt"
+)
+
+func main() {
+	arr := algorithms.GetArr(5, 20)
+	arr = []int{29, 10, 14, 37, 14}
+	fmt.Println("[UNSORTED]: ", arr)
+
+	n := len(arr)
+	if n <= 1 {
+		fmt.Println("[ALREADY SORTED]: ", arr)
+		return
+	}
+	// 遍历所有元素
+	for i := 1; i < n; i++ {
+		// 向前找位置
+		for j := i; j > 0; j-- {
+			// 合适位置插入
+			if arr[j-1] > arr[j] {
+				arr[j-1], arr[j] = arr[j], arr[j-1]
+			}
+		}
+		fmt.Println("[DEBUG]: ", arr)
+	}
+	fmt.Println("[SORTED]: ", arr)
+}
