1ad1455a3411937c49e2693d9aad9f7d76c9cf6e
wuYin <wuYinPost@gmail.com>
2018-06-21 18:53:31 +0800

    完成基本的有权有向图构建

--- graph/graph.go
+++ graph/graph.go
@@ -2,32 +2,41 @@ package graph
 
 import "fmt"
 
-type Graph struct {
-	Vertexes int
-	Edges    [][]int
+// 有向边
+type Edge struct {
+	From   int // 入节点
+	To     int // 出节点
+	Weight int // 权重
 }
 
-// 创建具有 n 个节点的无权、无向图
+type Graph struct {
+	Vertexes int
+	Edges    [][]Edge
+}
+
+// 创建具有 n 个节点的有权有向图
 func NewGraph(n int) *Graph {
 	return &Graph{
 		Vertexes: n,
-		Edges:    make([][]int, n),
+		Edges:    make([][]Edge, n),
 	}
 }
 
-// 向无向图中添加边
-func (g *Graph) AddEdge(u, v int) {
-	// 双向边的效果
-	g.Edges[u] = append(g.Edges[u], v)
-	g.Edges[v] = append(g.Edges[v], u)
+// 向有向图中添加边
+func (g *Graph) AddEdge(u, v, w int) {
+	g.Edges[u] = append(g.Edges[u], Edge{
+		From:   u,
+		To:     v,
+		Weight: w,
+	})
 }
 
 // 将邻接好的图输出
 func (g *Graph) AdjacentEdges() {
-	for u, vertexes := range g.Edges {
+	for _, vertexes := range g.Edges {
 		for _, v := range vertexes {
 			// 输出从 u 到 v 的边
-			fmt.Printf("[Eage]:\t%v -> %v\n", u, v)
+			fmt.Printf("[Eage]:\t%v -> %v(%d)\n", v.From, v.To, v.Weight)
 		}
 	}
 }
--- graph/graph_test.go
+++ graph/graph_test.go
@@ -6,9 +6,9 @@ import (
 
 func TestAdjacentEdges(t *testing.T) {
 	g := NewGraph(6)
-	g.AddEdge(1, 2)
-	g.AddEdge(1, 3)
-	g.AddEdge(1, 4)
-	g.AddEdge(3, 5)
+	g.AddEdge(1, 2, 1)
+	g.AddEdge(1, 3, 1)
+	g.AddEdge(1, 4, 1)
+	g.AddEdge(3, 5, 3)
 	g.AdjacentEdges()
 }
